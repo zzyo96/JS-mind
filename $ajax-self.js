0ff9e6cb309a1b84f436e1f8afefc4ad84b37837
zzyo96 <40533543+zzyo96@users.noreply.github.com>
2018-09-16 16:46:50 +0800

    Add files via upload

---
+++ $ajax-self.js
@@ -0,0 +1,49 @@
+window.jQuery = function(nodeOrSelector){
+  let nodes = {}
+  nodes.addClass = function(){}
+  nodes.html = function(){}
+  return nodes
+}
+window.$ = window.jQuery
+
+window.jQuery.ajax = function({url,method,body,successFn,failFn,headers}){
+  let request = new XMLHttpRequest()
+  request.open(method,url) //配置request
+  for(let key in headers){
+    let value = headers[key]
+    request.setRequestHeader(key,value)
+  }
+  request.onreadystatechange = ()=>{
+    if(request.readyState === 4){
+      if(request.status >=200 && request.status <300){
+        successFn.call(undefined,request.responseText)
+      }else if(request.status >= 400){
+        failFn.call(undefined,request)
+      }
+    }
+  }
+  request.send(body)
+}
+
+function f1(responseText){}
+function f2(responseText){}
+
+myButton.addEventListener('click',(e)=>{
+  window.jQuery.ajax({
+    url:'/xxx',
+    method:'get',
+    headers:{
+      'content-type':'application/x-www-form-urlencoded',
+      'jack':'18'
+    },
+    successFn:(x)=>{
+      f1.call(undefined,x)
+      f2.call(undefined,x)
+    },
+    failFn:(x)=>{
+      console.log(x)
+      console.log(x.status)
+      console.log(x.responseText)
+    }
+  })
+})
